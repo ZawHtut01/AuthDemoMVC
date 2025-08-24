4297ed5c29a54c7bf312f1f5eaa0e7a594dcb71c
Zaw Htut <zawhtut0010zha@gmail.com>
2025-08-24 13:28:25 +0630

    add product for develop branch

---
+++ AuthDemoMVC/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthDemoMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
