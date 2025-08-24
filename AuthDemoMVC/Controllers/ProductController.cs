cde982ad58bdae77863b520cf1ea460b12cc926e
Zaw Htut <zawhtut0010zha@gmail.com>
2025-08-24 13:33:38 +0630

    add test function in product controller

--- AuthDemoMVC/Controllers/ProductController.cs
+++ AuthDemoMVC/Controllers/ProductController.cs
@@ -8,5 +8,10 @@ namespace AuthDemoMVC.Controllers
         {
             return View();
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
